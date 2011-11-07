4a1742c7a756a6c757e9c60bdd7427ccc4c3140d
John Hren <johnhren@sfs-wifi-aruba-secure-dhcp-130-212-125-181.sfsu.edu>
2011-11-07 13:25:56 -0600

    inital code to submit file to sword service

--- rhaptos/swordclient/plone/browser.py
+++ rhaptos/swordclient/plone/browser.py
@@ -1,6 +1,7 @@
 from zope import interface, schema
 from zope.formlib import form
 from five.formlib import formbase
+from xml.dom.minidom import parseString
 
 class IDeposit(interface.Interface):
     text = schema.TextLine(title=u'Deposit URL',
@@ -8,12 +9,25 @@ class IDeposit(interface.Interface):
                            required=False)
 
 class Deposit(formbase.PageForm):
+    """Submit the contents of this page and its referenced images to a sword service
     """
-    """
-
     form_fields = form.FormFields(IDeposit)
-
+ 
     @form.action("Submit")
     def handle_submit(self):
-        pass
-
+	"""Assemble Sword submission, assumes content in AT Document"""
+        encoding = self.context.getCharset()
+        ptext = self.context.getRawText()
+	dom = parseString("<body>%s</body>% ptext)
+        remoteImages = []
+        localImages = []
+	for node in dom.getElementsByTagName("img"):
+	    src = node.getAttribute("img")
+	    src = src.encode('UTF-8')
+            if src.startsWith("http://"):
+		remoteImages.append(src)
+	    elseif src:
+		localImages.append(src)
+        
+	
+        """todo get images to create zip file for upload""" 		
