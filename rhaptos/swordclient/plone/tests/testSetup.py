e3fe6c36ec9f7f1f0109aba3ac9c01d64bdcb81c
Michael Davis <m.r.davis@cranfield.ac.uk>
2011-11-07 10:30:14 -0800

    Add test framework

---
+++ rhaptos/swordclient/plone/tests/testSetup.py
@@ -0,0 +1,29 @@
+import unittest2 as unittest
+
+from Products.CMFCore.utils import getToolByName
+
+from base import PROJECTNAME
+from base import INTEGRATION_TESTING
+
+class TestInstallation(unittest.TestCase):
+    """Ensure product is properly installed"""
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+class TestReinstall(unittest.TestCase):
+    """Ensure product can be reinstalled safely"""
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def testReinstall(self):
+        portal_setup = getToolByName(self.portal, 'portal_setup')
+        try:
+            portal_setup.runAllImportStepsFromProfile('profile-%s:default' % PROJECTNAME)
+        except BadRequest:
+            # if tests run too fast, duplicate profile import id makes test fail
+            time.sleep(0.5)
+            portal_setup.runAllImportStepsFromProfile('profile-%s:default' % PROJECTNAME)
