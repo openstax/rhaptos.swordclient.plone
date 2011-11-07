d5a3cdaf7e21937a55de9c0401c94c096c8fa0c3
Françoise Conil <fcodvpt@gmail.com>
2011-11-07 10:57:39 -0800

    Improved the depostit form with a template.

--- rhaptos/swordclient/plone/browser.py
+++ rhaptos/swordclient/plone/browser.py
@@ -1,17 +1,27 @@
 from zope import interface, schema
 from zope.formlib import form
 from five.formlib import formbase
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class IDeposit(interface.Interface):
     text = schema.TextLine(title=u'Deposit URL',
                            description=u'Deposit the current page to the URL',
-                           required=False)
+                           required=True)
+
+    username = schema.TextLine(title=u'Username',
+                           description=u'Username',
+                           required=True)
+
+    password = schema.TextLine(title=u'Password',
+                           description=u'Password',
+                           required=True)
 
 class Deposit(formbase.PageForm):
     """
     """
 
     form_fields = form.FormFields(IDeposit)
+    template = ViewPageTemplateFile('deposit.pt')
 
     @form.action("Submit")
     def handle_submit(self):
