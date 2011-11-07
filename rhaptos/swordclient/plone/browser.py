10fea5ccb58955da2ea1299c8fec2bcc083c738e
Françoise Conil <fcodvpt@gmail.com>
2011-11-06 18:15:54 -0800

    Added a formlib form for deposit.

--- rhaptos/swordclient/plone/browser.py
+++ rhaptos/swordclient/plone/browser.py
@@ -1,11 +1,19 @@
+from zope import interface, schema
 from zope.formlib import form
-from Products.Five import BrowserView
+from five.formlib import formbase
 
-class Deposit(BrowserView):
+class IDeposit(interface.Interface):
+    text = schema.TextLine(title=u'Deposit URL',
+                           description=u'Deposit the current page to the URL',
+                           required=False)
+
+class Deposit(formbase.PageForm):
     """
     """
 
-    @ form.action("Submit")
+    form_fields = form.FormFields(IDeposit)
+
+    @form.action("Submit")
     def handle_submit(self):
         pass
 
